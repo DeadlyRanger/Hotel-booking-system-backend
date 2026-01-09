d92c5180a4a92d5736fd3a7ae5a512d327a4f557
Deadlyranger <muditkubrey@gmail.com>
2026-01-09 22:32:04 +0530

    cors working added '

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hotel-booking-system-frontend-eight.vercel.app",
     credentials: true
   })
 );
