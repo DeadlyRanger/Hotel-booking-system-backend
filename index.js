0111f3a12224240f61cd771cac10518676839fc9
Deadlyranger <muditkubrey@gmail.com>
2026-01-09 22:23:15 +0530

    health route added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+dotenv.config();
 
 import dbConnect from "./config/db.js";
 import router from "./routes/Listing.routes.js";
@@ -8,7 +10,7 @@ import userRouter from "./routes/User.routes.js";
 import bookingRouter from "./routes/Booking.routes.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 // 🔥 MIDDLEWARE ORDER MATTERS
 app.use(cookieParser());
@@ -26,6 +28,9 @@ app.use(express.json());
 app.use("/api/listings", router);
 app.use("/api/user", userRouter);
 app.use("/api/listing", bookingRouter);
+app.use('/api/healthy',(req,res)=>{
+  res.send('working fine ')
+})
 
 // SERVER
 app.listen(port, () => {
